42d860e88c43ce012740f6cc53d0d5b66089f5ca
Qasim <qasimrehman@Qasims-MacBook-Pro.local>
2021-06-16 13:18:52 +0500

    first commit

---
+++ OsInfo.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const getOSinformation = () => {
+  const osInfo = {
+    hostname: os.hostname(), 
+    type: os.type(), 
+    platform: os.platform(), 
+    cpu: os.cpus(), 
+    architechure: os.arch(), 
+    homedir: os.homedir(), 
+  }
+  return osInfo;
+}
+
+const writeOSinfo = () => {
+  const osInfo = getOSinformation();
+  const {homedir} = osInfo;
+  fs.writeFileSync(
+    path.join(homedir+'/'+process.env.FILE_DIRECTORY+'/'+process.env.FILE_NAME), 
+    JSON.stringify(osInfo), 
+    (err) => {
+      console.log(err);
+    }
+  );
+}
+
+
+writeOSinfo();
